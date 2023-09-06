415c834f5868ee4e940b66118e1968cd7ccdad52
unknown <ido.bandd@gmail.com>
2023-09-06 12:53:36 +0300

    code splitting with 'lazy' and 'Suspense' from React

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,65 @@
 import Navbar from './components/Navbar/Navbar'
-
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+import { ReactNode, lazy, Suspense } from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Fallback from './components/Fallback/Fallback'
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <AppLayout>
+        <Suspense fallback={<Fallback />} >
+          <Routes>
+            {ROUTES.map(route => {
+              return <Route path={route.path} element={<route.element />} key={route.path + 'route'} />
+            })}
+          </Routes>
+        </Suspense>
+      </AppLayout>
+    </BrowserRouter>
   )
 }
 
 export default App
+
+const ROUTES = [
+  {
+      path: '/',
+      element: lazy(() => import('./pages/Home/Home'))
+  },
+  {
+      path: '/RandomPlant',
+      element: lazy(() => import('./pages/RandomPlant/RandomPlant'))
+  },
+  {
+      path: '/MyGarden',
+      element: lazy(() => import('./pages/MyGarden/MyGarden'))
+  },
+  {
+      path: '/PlantTimeline/:id',
+      element: lazy(() => import('./pages/PlantTimeline/PlantTimeline'))
+  },
+  {
+      path: '/IdentifyPlant',
+      element: lazy(() => import('./pages/IdentifyPlant/IdentifyPlant'))
+  },
+  {
+      path: '/Community',
+      element: lazy(() => import('./pages/Community/Community'))
+  },
+]
+
+const AppLayout = ({children}: AppLayoutProps) => {
+  return (
+    <>
+      <Navbar />
+        {children}
+        <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+type AppLayoutProps = {
+  children: ReactNode
+}
---
+++ src/components/Fallback/Fallback.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Fallback = () => {
+  return (
+    <div className='fallback'>
+
+    </div>
+  )
+}
+
+export default Fallback
